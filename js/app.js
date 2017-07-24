d4b8414aeb690060260643efb87c231e813e408a
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2017-07-23 23:31:09 -1000

    completed exercise 5

--- js/app.js
+++ js/app.js
@@ -66,6 +66,11 @@ function valueMenu(){
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 20px after clicking on the text.*/
 
+function redFace(){
+	document.getElementById("drink").style.fontSize = "20px";
+	document.getElementById("drink").style.backgroundColor = "red";
+}
+
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
