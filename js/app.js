95e36337edf047c69ba6e5dbabd4d7663364619c
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2017-07-23 23:06:41 -1000

    completed exercise 3

--- js/app.js
+++ js/app.js
@@ -37,6 +37,14 @@ function lessContent(){
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
+var zoomBiggie = document.getElementById("biggie");
+console.log(zoomBiggie);
+
+function zoom(){
+	zoomBiggie.style.fontSize = "150%";
+}
+
+
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
